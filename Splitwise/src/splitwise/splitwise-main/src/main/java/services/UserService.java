aded7e40498ad180c9fa1dcfe3e61bbeb1eb4aca
RACHA <rachakondasteja@gmail.com>
2026-02-04 19:58:30 +0530

    DATA-0987

--- Splitwise/src/splitwise/splitwise-main/src/main/java/services/UserService.java
+++ Splitwise/src/splitwise/splitwise-main/src/main/java/services/UserService.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class UserService {
+public class   UserService {
 
     private GroupRepository groupRepository;
     private UserExpenseRepository userExpenseRepository;
