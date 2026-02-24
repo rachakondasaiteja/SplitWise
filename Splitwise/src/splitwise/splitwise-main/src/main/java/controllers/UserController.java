293474a9190dbced317ef85a34fa5b8e8198cbb6
RACHA <rachakondasteja@gmail.com>
2026-02-24 16:51:20 +0530

    DATA-09870 Added space in UserController.java

--- Splitwise/src/splitwise/splitwise-main/src/main/java/controllers/UserController.java
+++ Splitwise/src/splitwise/splitwise-main/src/main/java/controllers/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
 
     private UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService   userService) {
         this.userService = userService;
     }
 
