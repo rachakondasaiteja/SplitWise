24dd6d97145da84e049c7fdd88b2a40c413f58db
RACHA <rachakondasteja@gmail.com>
2026-02-24 16:56:10 +0530

    DATA-098765 Added space in Transaction.java

--- Splitwise/src/splitwise/splitwise-main/src/main/java/dtos/Transaction.java
+++ Splitwise/src/splitwise/splitwise-main/src/main/java/dtos/Transaction.java
@@ -7,7 +7,7 @@ public class Transaction {
     private int amount;
 
     public Transaction(String from, String to, int amount) {
-        this.from = from;
+        this.from =   from;
         this.to = to;
         this.amount = amount;
     }
